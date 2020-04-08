48ddd41274542b56d8631de3aa4f14dd7109795b
Tuskub <56831532+Tuskub@users.noreply.github.com>
2020-04-08 17:16:40 +0500

    Первая реализация бота погоды в Оренбурге

--- Observer/bot.py
+++ Observer/bot.py
@@ -1,8 +1,19 @@
-import json
+# Стандартные библиотеки
+import time
+from threading import Thread
+# Библиотека для выполнения команд в определенное время
+import schedule
+# Библиотеки для работы с Telegram API
 import telebot
 from telebot import apihelper
+# Константы
 from constants.proxyconst import USERNAME, PASSWORD, IP, PORT
 from constants.bottoken import TOKEN
+# Классы из реализации наблюдателя
+from Observer.weather import WeatherData
+from Observer.subscribers.current_conditions import CurrentConditions
+from Observer.subscribers.statistic import WeatherStatistics
+from Observer.subscribers.forecast import Forecast
 
 
 bot = telebot.TeleBot(TOKEN)
@@ -14,19 +25,60 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Бот погоды в Оренбурге!')
 
 
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    help_info = 'Данный бот предназначен для вывода информации о погоде в Оренбурге!\n' \
+                'Список команд:\n' \
+                '\'Погода\' - для вывода информации актуальной информации о погоде\n' \
+                '\'Статистика\' - для вывода статистической информации о температуре\n' \
+                '\'Прогноз\' - для получения прогноза погоды на ближайшие полчаса\n'
+    bot.send_message(message.chat.id, help_info)
+
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    with open('response.json') as f:
-        templates = json.load(f)
-    print(templates)
+    print(message.chat.id)
     if message.text == 'Погода':
-        bot.send_message(message.chat.id, f"Погода в Оренбурге: "
-                                          f"\nТемпература: {templates['fact']['temp']}\xb0С"
-                                          f"\nОщущается как: {templates['fact']['feels_like']}\xb0С"
-                                          f"\nСкорость ветра: {templates['fact']['wind_speed']}м/с"
-                                          f"\nКртинОчка:  https://yastatic.net/weather/i/icons/blueye/color/svg/{templates['fact']['icon']}.svg")
-    elif message.text == 'Пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
+        bot.send_message(message.chat.id, cs.display())
+    elif message.text == 'Статистика':
+        bot.send_message(message.chat.id, stat.display())
+    elif message.text == 'Прогноз':
+        bot.send_message(message.chat.id, forecast.display())
 
 
-bot.polling()
+def start_bot():
+    bot.polling(none_stop=True)
+
+
+def update_weather():
+    wd.measurements_update()
+    # bot.send_message(394068509, 'Саня')
+    # bot.send_message(430265734, 'Женя')
+
+
+def update_data():
+    schedule.every().hour.at(":00").do(update_weather)
+    schedule.every().hour.at(":30").do(update_weather)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    wd = WeatherData()
+    cs = CurrentConditions()
+    stat = WeatherStatistics()
+    forecast = Forecast()
+
+    wd.register_observer(cs)
+    wd.register_observer(stat)
+    wd.register_observer(forecast)
+
+    t1 = Thread(target=start_bot)
+    t2 = Thread(target=update_data)
+
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
---
+++ Observer/weather.py
@@ -0,0 +1,44 @@
+import json
+import requests
+from typing import List
+from Observer.constants.apikey import YA_WEATHER_KEY, LAT, LON
+from Observer.interfases.subject import Subject
+from Observer.interfases.observer import Observer
+
+
+class WeatherData(Subject):
+    __current_state = None
+    __subscribers: List[Observer] = []
+
+    def __init__(self):
+        self.__update_current_state()
+        return
+
+    def register_observer(self, subscriber):
+        self.__subscribers.append(subscriber)
+        return
+
+    def remove_observer(self, subscriber):
+        self.__subscribers.remove(subscriber)
+        return
+
+    def notify_observer(self):
+        for s in self.__subscribers:
+            s.update()
+        return
+
+    @staticmethod
+    def __update_current_state():
+        res = requests.get(f'https://api.weather.yandex.ru/v1/informers?lat={LAT}&lon={LON}',
+                           headers={'X-Yandex-API-Key': YA_WEATHER_KEY})
+        res.raise_for_status()
+        res.raw.decode_content = True
+        data = res.json()
+        with open('current_weather.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
+        return
+
+    def measurements_update(self):
+        self.__update_current_state()
+        self.notify_observer()
+        return
